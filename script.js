472be441ae9850a576a95fe88ebe6377dd8d5a91
dentease <dentease.info@gmail.com>
2025-01-06 10:11:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ async function getNearbyDentists() {
 
     navigator.geolocation.getCurrentPosition(async (position) => {
         const { latitude, longitude } = position.coords;
-        const apiKey = "LA_TUA_API_KEY"; // Sostituisci con la tua API Key di Google
+        const apiKey = "AIzaSyDWJb7ndaGTMoVqT3FuVIhE_XbVgTxOMwE"; // Sostituisci con la tua API Key di Google
         const radius = 5000; // Raggio in metri
         const type = "dentist";
 
